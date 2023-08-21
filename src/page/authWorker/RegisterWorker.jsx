6262c29c85a3596be3396774078b174f5f3c3a2d
Mahardhika Pratama <putrad578@gmail.com>
2023-08-21 13:53:09 +0700

    integrated api login/register worker

--- src/page/authWorker/RegisterWorker.jsx
+++ src/page/authWorker/RegisterWorker.jsx
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { register } from "../../redux/action/worker";
+import { ToastContainer } from 'react-toastify';
 
 import bannerPhoto from "../../assets/image/bannerPhoto.png";
 import Logo1 from "../../assets/image/logo1.png";
 import "../../assets/css/main.css";
 
 const RegisterWorker = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [form, setForm] = useState({
     name: "",
     email: "",
-    hoPhone: "",
+    phone: "",
     password: "",
+    confirm_password: ""
   });
+
+  const postDataRegister = async (e) => {
+    e.preventDefault();
+    dispatch(register(form, navigate))
+  }
+
+  const onChangeRegister = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+    console.log('Register Worker', form)
+  }
 
   return (
     <div>
@@ -21,7 +37,7 @@ const RegisterWorker = () => {
           <Col md="6" className="">
             <Card className="border-0">
               <div className="bannerPhoto">
-                <img src={bannerPhoto} alt="" srcset="" />
+                <img src={bannerPhoto} alt="" srcSet="" />
               </div>
               <div className="card-img-overlay">
                 <div className="ps-4 mt-3">
@@ -40,26 +56,26 @@ const RegisterWorker = () => {
               <h3>Halo, Pewpeople</h3>
               <p className="text-desc fw-lighter">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.</p>
 
-              <Form>
+              <Form onSubmit={postDataRegister}>
                 <Form.Group className="mb-3">
                   <Form.Label>Nama</Form.Label>
-                  <Form.Control type="text" className="py-3" onChange={(e) => setForm({ ...form, name: e.target.value })} placeholder="Masukan nama panjang" />
+                  <Form.Control type="text" name="name" onChange={onChangeRegister} value={form.name} className="py-3" placeholder="Masukan nama panjang" />
                 </Form.Group>
                 <Form.Group className="mb-3">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" className="py-3" onChange={(e) => setForm({ ...form, email: e.target.value })} placeholder="Masukan alamat email" />
+                  <Form.Control type="email" name="email" onChange={onChangeRegister} value={form.email} className="py-3" placeholder="Masukan alamat email" />
                 </Form.Group>
                 <Form.Group className="mb-3">
                   <Form.Label>No handphone</Form.Label>
-                  <Form.Control type="number" className="py-3" onChange={(e) => setForm({ ...form, hoPhone: e.target.value })} placeholder="Masukkan no handphone" />
+                  <Form.Control type="number" name="phone" onChange={onChangeRegister} value={form.phone} className="py-3" placeholder="Masukkan no handphone" />
                 </Form.Group>
                 <Form.Group className="mb-3">
                   <Form.Label>Kata Sandi</Form.Label>
-                  <Form.Control type="password" className="py-3" onChange={(e) => setForm({ ...form, password: e.target.value })} placeholder="Masukan kata sandi" />
+                  <Form.Control type="password" name="password" onChange={onChangeRegister} value={form.password} className="py-3" placeholder="Masukan kata sandi" />
                 </Form.Group>
                 <Form.Group className="mb-5">
                   <Form.Label>Konfirmasi kata sandi</Form.Label>
-                  <Form.Control type="password" className="py-3" placeholder="Masukan konfirmasi kata sandi" />
+                  <Form.Control type="password" name="confirm_password" onChange={onChangeRegister} value={form.confirm_password} className="py-3" placeholder="Masukan konfirmasi kata sandi" />
                 </Form.Group>
                 <div>
                   <Button type="submit" variant="warning" className="d-block w-100 py-3 text-white fw-bold">
@@ -79,6 +95,7 @@ const RegisterWorker = () => {
           </Col>
         </Row>
       </Container>
+      <ToastContainer/>
     </div>
   );
 };
